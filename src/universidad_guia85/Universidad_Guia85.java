15f493e65a9cb34a13c2a6d15393d09f5e37d169
VGuilleRodriguez <guillermovrodriguez@gmail.com>
2023-09-11 19:09:13 -0300

    Clase MateriaData

--- src/universidad_guia85/Universidad_Guia85.java
+++ src/universidad_guia85/Universidad_Guia85.java
@@ -1,10 +1,18 @@
 
 package universidad_guia85;
 
+import java.time.LocalDate;
+import java.time.Month;
+import universidad_guia85.accesoadatos.AlumnoData;
+import universidad_guia85.entidades.Alumno;
+
 public class Universidad_Guia85 {
 
     public static void main(String[] args) {
 
+        Alumno Pedro = new Alumno(23456789,"Perez","Pedro",LocalDate.of(1980, 4, 15),true);
+        AlumnoData alu = new AlumnoData();
+        alu.guardarAlumno(Pedro);
     }
     
 }
